c23191921c96b6df3fba7e8c20fd91cdb1c64924
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-05 20:58:45 -0300

    Ajuste de porta do servidor

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get('/testando', (req, res) => {
     res.send("Teste deu certo!")
 })
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || process.env.PORT_APP || 3000;
 app.listen(PORT, function(){
   console.log(`Servidor rodando em: http://localhost:${PORT}`);
 });
