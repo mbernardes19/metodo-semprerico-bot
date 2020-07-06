01d06dd56afa957095fadb19f4a97980eed0f293
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-06 10:56:19 -0300

    Adiciona conexao com banco de dados

---
+++ db.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+
+const dbConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+})
+
+module.exports = {
+    conexao: dbConnection
+}
