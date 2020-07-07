3f213b9b573435c809992ca428863196f66e7408
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-07 18:42:15 -0300

    Adiciona configuração de Webpack para fazer a transpilação da aplicação

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const nodeExternals = require('webpack-node-externals')
+const path = require('path');
+
+module.exports = {
+  target: 'node',
+  externals: [nodeExternals({
+    whitelist:['telegraf']
+  })],
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: "app.js",
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      loader: 'babel-loader',
+      query: {
+        presets: [
+          ["env", {
+            "targets": {
+              "node": "current"
+            }
+          }]
+        ]
+      }
+    }]
+  },
+  resolve: {
+    extensions: ['.json', '.js']
+  }
+};
