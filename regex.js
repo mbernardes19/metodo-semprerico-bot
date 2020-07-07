8a17cc47d5784d819757a1634e036ebb640b9aee
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-06 22:38:15 -0300

    Adiciona novos padrões regex para conversas

--- regex.js
+++ regex.js
@@ -1,6 +1,9 @@
-const CARTAO = /^((cart[a|ã]o)|(cart[a|ã]o de cr[e|é]dito))$/gi
-const BOLETO = /(boleto)/
+const CARTAO = /^((cart[a|ã]o)|(cart[a|ã]o de cr[e|é]dito))$/i
+const BOLETO = /bolet[a|o]/i
+const SIM = /sim/i
+const NAO = /n[a|ã]o/i
+
 
 module.exports = {
-    CARTAO, BOLETO
+    CARTAO, BOLETO, SIM, NAO
 }
