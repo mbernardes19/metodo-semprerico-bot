dca1bfdb644773c3020dd3af751bb19a5cdcad3f
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-12 20:16:29 -0300

    Cria serviço de envio de email

---
+++ src/email.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer')
+const path = require('path')
+
+const transportador = nodemailer.createTransport({
+    host: process.env.SERVIDOR_SMTP,
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.USUARIO_EMAIL,
+        pass: process.env.SENHA_EMAIL
+    }
+})
+
+const enviarEmail = async (options={de, para, assunto, texto, anexos}) => {
+    const {de, para, assunto, texto, anexos} = options
+    const info = await transportador.sendMail({
+        from: de,
+        to: para,
+        subject: assunto,
+        text: texto,
+        attachments: anexos
+    })
+    console.log(info)
+}
+
+const enviarCSVParaEmail = async () => {
+    enviarEmail({
+        de: process.env.USUARIO_EMAIL,
+        para: process.env.EMAIL_PARA,
+        assunto: 'CSV com todos os usuários atualizado!',
+        texto: 'Segue um CSV com a sua base de usuários atual',
+        anexos: [{path: path.join(__dirname, '..', 'csv', 'usuarios.csv')}]
+    })
+}
+
+module.exports = { enviarCSVParaEmail }
