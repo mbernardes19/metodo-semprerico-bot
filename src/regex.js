6d2c47703780188861bd771cacd1e3797063d419
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-12 20:19:49 -0300

    Adiciona validação a email e telefone

--- src/regex.js
+++ src/regex.js
@@ -1,9 +1,11 @@
 const CARTAO = /^((cart[a|ã]o)|(cart[a|ã]o de cr[e|é]dito))$/i
 const BOLETO = /bolet[a|o]/i
-const SIM = /sim/i
-const NAO = /n[a|ã]o/i
+const SIM = /^(sim|s)$/i
+const NAO = /^(n[a|ã]o|n)$/i
+const TELEFONE = /(^\d{11}$)/
+const EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 
 module.exports = {
-    CARTAO, BOLETO, SIM, NAO
+    CARTAO, BOLETO, SIM, NAO, TELEFONE, EMAIL
 }
--- src/validacao.js
+++ src/validacao.js
@@ -1,7 +1,24 @@
 const regex = require('./regex')
 
+const validar = (informacao, dado) => {
+    switch(informacao) {
+        case 'telefone':
+            return regex.TELEFONE.test(dado.replace(/ /g, "")) ?
+                {temErro: false, mensagemDeErro: ""} :
+                {temErro: true, mensagemDeErro: "Telefone inválido. Certifique-se de estar inserindo somente números e o DDD (sem o zero) junto."}
+        case 'email':
+            return regex.EMAIL.test(dado) ?
+                {temErro: false, mensagemDeErro: ""} :
+                {temErro: true, mensagemDeErro: "Email inválido. Certifique-se de estar inserindo um email válido."}
+        default: return {temErro: false, mensagemDeErro: ""}
+    }
+}
+
 module.exports = {
     formaDePagamentoValida: (ctx) => regex.CARTAO.test(ctx.message.text) || regex.BOLETO.test(ctx.message.text),
+    cartao: (ctx) => regex.CARTAO.test(ctx.message.text),
+    boleto: (ctx) => regex.BOLETO.test(ctx.message.text),
     confirmado: (ctx) => regex.SIM.test(ctx.message.text),
-    negado: (ctx) => regex.NAO.test(ctx.message.text)
+    negado: (ctx) => regex.NAO.test(ctx.message.text),
+    validar
 }
