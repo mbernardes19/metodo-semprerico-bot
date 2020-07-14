381b221ceb72c38a188e4eaf8314cb559be8d4cb
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-14 20:37:52 -0300

    Trata todos os erros de banco de dados

--- src/dao.js
+++ src/dao.js
@@ -3,23 +3,44 @@ const util = require('util')
 const adicionarUsuarioAoBancoDeDados = async (usuario, conexao) => {
     const {idTelegram, nomeCompleto, formaDePagamento, email, telefone, statusAssinatura} = usuario
     const query = util.promisify(conexao.query).bind(conexao)
-    await query(`insert into Usuario (id, nome_completo, telefone, email, forma_de_pagamento, status_assinatura) values ('${idTelegram}', '${nomeCompleto}', '${telefone}', '${email}', '${formaDePagamento}', '${statusAssinatura}')`)
+    try {
+        await query(`insert into Usuario (id, nome_completo, telefone, email, forma_de_pagamento, status_assinatura) values ('${idTelegram}', '${nomeCompleto}', '${telefone}', '${email}', '${formaDePagamento}', '${statusAssinatura}')`)
+    } catch (err) {
+        throw err
+    }
 }
 
 const limparBancoDeDados = async (conexao) => {
     const query = util.promisify(conexao.query).bind(conexao)
-    await query(`truncate table Usuario`)
-    await query(`truncate table EmailBloqueado`)
+    try {
+        await query(`truncate table Usuario`)
+        await query(`truncate table EmailBloqueado`)
+    }
+    catch (err) {
+        throw err
+    }
 }
 
 const adicionarEmEmailsBloqueados = async (email, conexao) => {
     const query = util.promisify(conexao.query).bind(conexao)
-    await query(`insert into EmailBloqueado values ('${email}')`)
+    try {
+        const emailsBloqueados = await query(`select * from EmailBloqueado`)
+        const emailsIguais = emailsBloqueados.filter(emailBloqueado => emailBloqueado.email === email)
+        if (emailsIguais.length === 0) {
+            await query(`insert into EmailBloqueado values ('${email}')`)
+        }
+    } catch (err) {
+        throw err
+    }
 }
 
 const pegarTodosUsuariosDoBancoDeDados = async (conexao) => {
     const query = util.promisify(conexao.query).bind(conexao)
-    return await query(`select * from Usuario`)
+    try {
+        return await query(`select * from Usuario`)
+    } catch (err) {
+        throw err;
+    }
 }
 
 const atualizarStatusDeAssinaturaDeUsuarios = async (usuarios, novosStatus, conexao) => {
@@ -28,7 +49,11 @@ const atualizarStatusDeAssinaturaDeUsuarios = async (usuarios, novosStatus, cone
     usuarios.forEach((usuario, index) => {
         queries.push(query(`update Usuario set status_assinatura='${novosStatus[index]}' where id=${usuario.id}`))
     })
-    await Promise.all(usuarios)
+    try {
+        await Promise.all(usuarios)
+    } catch (err) {
+        throw err;
+    }
 }
 
 module.exports = {
