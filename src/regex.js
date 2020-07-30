274193d09dd31f8d2c97509e8b22dc9c9b3a93b6
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-29 23:53:53 -0300

    Atualiza regex de sinal

--- src/regex.js
+++ src/regex.js
@@ -4,7 +4,7 @@ const SIM = /^(sim|s)$/i
 const NAO = /^(n[a|ã]o|n)$/i
 const TELEFONE = /(^\d{11}$)/
 const EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const SINAL = /((?<=Par - )\w{6})|((?<=Ordem - )[^\\]*)|((?<=Horário 🕙 - )\d{2}:\d{2})/gm
+const SINAL = /((?<=Par - )\w{6})|((?<=Ordem - )[^\\]*)|((?<=Horário - )\d{2}:\d{2})/gm
 
 module.exports = {
     CARTAO, BOLETO, SIM, NAO, TELEFONE, EMAIL, SINAL
