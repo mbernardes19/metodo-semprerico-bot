7124c29bf6f4226357042d7df0da491552ca9664
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-30 21:31:10 -0300

    Correcao de erro em regex

--- src/regex.js
+++ src/regex.js
@@ -4,7 +4,7 @@ const SIM = /^(sim|s)$/i
 const NAO = /^(n[a|ã]o|n)$/i
 const TELEFONE = /(^\d{11}$)/
 const EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const SINAL = /((?<=Par - )\w{6})|((?<=Ordem - )[^\\]*)|((?<=Horário - )\d{2}:\d{2})/gm
+const SINAL = /((?<=Par - )\w{6})|((?<=Ordem - )\w*)|((?<=Horário - )\d{2}:\d{2})/gm
 
 module.exports = {
     CARTAO, BOLETO, SIM, NAO, TELEFONE, EMAIL, SINAL
