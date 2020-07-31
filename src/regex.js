f8e8c92074714faa09dc687bc932e7a6a33f8ec5
mbernardes19 <bernardes.matheus@outlook.com>
2020-07-31 19:02:49 -0300

    Atualiza regex SINAL para tratar Pares OTC

--- src/regex.js
+++ src/regex.js
@@ -4,7 +4,7 @@ const SIM = /^(sim|s)$/i
 const NAO = /^(n[a|ã]o|n)$/i
 const TELEFONE = /(^\d{11}$)/
 const EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-const SINAL = /((?<=Par - )\w{6})|((?<=Ordem - )\w*)|((?<=Horário - )\d{2}:\d{2})/gm
+const SINAL = /((?<=Par - )[A-Z]{6}\W[A-Z]{3}|[A-Z]{6})|((?<=Ordem - )\w*)|((?<=Horário - )\d{2}:\d{2})/gm
 
 module.exports = {
     CARTAO, BOLETO, SIM, NAO, TELEFONE, EMAIL, SINAL
