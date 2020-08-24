1a10f70988c0911f634805abdeb63f1fbaaf6662
mbernardes19 <bernardes.matheus@outlook.com>
2020-08-24 02:29:04 -0300

    Adds health checker server

---
+++ health-checker.js
@@ -0,0 +1,20 @@
+const app = require('express')()
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runDeploy() {
+    try {
+        const {stdout, stderr} = await exec('bash ../../deploy.sh')
+        console.log('stdout', stdout)
+        console.log('stderr', stderr)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+app.get('/revive', async (req, res) => {
+    await runDeploy()
+})
+
+const PORT = process.env.PORT_METODO_SEMPRERICO_BOT_APP || process.env.PORT_APP || 3000
+app.listen(PORT, () => log(`Health checker rodando na porta ${PORT}`));
