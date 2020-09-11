32042e131c760c0c555cc98fa4ea8a6505abcbc2
mbernardes19 <bernardes.matheus@outlook.com>
2020-09-11 20:29:23 -0300

    Refatora cena de forma de pagamento

---
+++ src/cenas/formaPagamento.js
@@ -0,0 +1,70 @@
+const Composer = require('telegraf/composer')
+const { log } = require('../servicos/logger')
+const { boleto, cartao, planoGratuito } = require('../servicos/validacao')
+
+const pedirFormaDePagamento = new Composer()
+pedirFormaDePagamento.action('cartao_de_credito', async (ctx) => {
+    await ctx.answerCbQuery()
+    ctx.wizard.state.novoUsuario.formaDePagamento = 'cartao_de_credito'
+    log('Forma de pagamento definida')
+    await ctx.reply('Certo!')
+    await ctx.reply('Vou precisar de mais alguns dados pra confirmar o pagamento no servidor da Monetizze, tudo bem?')
+    await ctx.reply('Qual é o seu nome completo?')
+    return ctx.wizard.next()
+  })
+pedirFormaDePagamento.action('boleto', async (ctx) => {
+    await ctx.answerCbQuery()
+    ctx.wizard.state.novoUsuario.formaDePagamento = 'boleto'
+    log('Forma de pagamento definida')
+    await ctx.reply('Certo!')
+    await ctx.reply('Vou precisar de mais alguns dados pra confirmar o pagamento no servidor da Monetizze, tudo bem?')
+    await ctx.reply('Qual é o seu nome completo?')
+    return ctx.wizard.next()
+})
+pedirFormaDePagamento.action('plano_gratuito', async (ctx) => {
+    await ctx.answerCbQuery()
+    ctx.wizard.state.novoUsuario.formaDePagamento = 'plano_gratuito'
+    log('Forma de pagamento definida')
+    await ctx.reply('Certo!')
+    await ctx.reply('Vou precisar de alguns dados para liberar seu período gratuito de 1 mês nos nossos canais VIPs do Método Sempre Rico!')
+    await ctx.reply('Qual é o seu nome completo?')
+    return ctx.wizard.next()
+})
+pedirFormaDePagamento.use(async (ctx) => {
+    if (cartao(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        ctx.wizard.state.novoUsuario.formaDePagamento = 'cartao_de_credito'
+        log('Forma de pagamento definida')
+        await ctx.reply('Certo!')
+        await ctx.reply('Vou precisar de mais alguns dados pra confirmar o pagamento no servidor da Monetizze, tudo bem?')
+        await ctx.reply('Qual é o seu nome completo?')
+        return ctx.wizard.next()
+    }
+    if (boleto(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        ctx.wizard.state.novoUsuario.formaDePagamento = 'boleto'
+        log('Forma de pagamento definida')
+        await ctx.reply('Certo!')
+        await ctx.reply('Vou precisar de mais alguns dados pra confirmar o pagamento no servidor da Monetizze, tudo bem?')
+        await ctx.reply('Qual é o seu nome completo?')
+        return ctx.wizard.next()
+    }
+    if (planoGratuito(ctx)) {
+        if (!ctx.message) {
+            await ctx.answerCbQuery()
+        }
+        ctx.wizard.state.novoUsuario.formaDePagamento = 'plano_gratuito'
+        log('Plano gratuito definido')
+        await ctx.reply('Certo!')
+        await ctx.reply('Vou precisar de alguns dados para liberar seu período gratuito de 1 mês nos nossos canais VIPs do Método Sempre Rico!')
+        await ctx.reply('Qual é o seu nome completo?')
+        return ctx.wizard.next()
+    }
+    await ctx.reply('Por favor, escolha uma das opções.')
+})
+
+module.exports = { pedirFormaDePagamento }
