81b29f7a1f99b3ef986a38c6710014842f333177
mbernardes19 <bernardes.matheus@outlook.com>
2020-09-21 10:28:03 -0300

    Faz com que usuário gratuito seja desbanido ao entrar como usuário pago

--- src/passos/planoPago/telefone.js
+++ src/passos/planoPago/telefone.js
@@ -1,73 +1,73 @@
-const Composer = require('telegraf/composer')
-const Markup = require('telegraf/markup')
-const Extra = require('telegraf/extra')
-const { log } = require('../../servicos/logger')
-const { confirmado, negado, validar } = require('../../servicos/validacao')
+const Composer = require('telegraf/composer');
+const Markup = require('telegraf/markup');
+const Extra = require('telegraf/extra');
+const { log } = require('../../servicos/logger');
+const { confirmado, negado, validar } = require('../../servicos/validacao');
 
 const confirmacaoPositiva = async (ctx) => {
-    const validacao = validar('telefone', ctx.wizard.state.novoUsuario.telefone);
-    if (!validacao.temErro) {
-        await ctx.reply('Confirmado!')
-        await ctx.reply('Qual é o seu email?')
-        return ctx.wizard.next()
-    }
-    await ctx.reply(`${validacao.mensagemDeErro}`)
-    await ctx.reply(`Por favor, insira seu telefone novamente`)
-    return ctx.wizard.back()
-}
+  const validacao = validar('telefone', ctx.wizard.state.novoUsuario.telefone);
+  if (!validacao.temErro) {
+    await ctx.reply('Confirmado!');
+    await ctx.reply('Qual é o seu email?');
+    return ctx.wizard.next();
+  }
+  await ctx.reply(`${validacao.mensagemDeErro}`);
+  await ctx.reply('Por favor, insira seu telefone novamente');
+  return ctx.wizard.back();
+};
 
 const pegarTelefone = async (ctx) => {
-    if (!ctx.message) {
-        await ctx.answerCbQuery()
-    }
-    try {
-        const confirmacao = Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')])
-        await ctx.reply(`Confirmando, seu telefone é ${ctx.message.text}, certo?`, Extra.inReplyTo(ctx.message.message_id).markup(confirmacao))
-        ctx.wizard.state.novoUsuario.telefone = ctx.message.text.replace(/ /g, "")
-        log(`Telefone definido`)
-        log(ctx.wizard.state.novoUsuario)
-        ctx.wizard.next()
-    } catch (err) {
-        ctx.scene.leave()
-    }
-}
+  if (!ctx.message) {
+    await ctx.answerCbQuery();
+  }
+  try {
+    const confirmacao = Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')]);
+    await ctx.reply(`Confirmando, seu telefone é ${ctx.message.text}, certo?`, Extra.inReplyTo(ctx.message.message_id).markup(confirmacao));
+    ctx.wizard.state.novoUsuario.telefone = ctx.message.text.replace(/ /g, '');
+    log('Telefone definido');
+    log(ctx.wizard.state.novoUsuario);
+    ctx.wizard.next();
+  } catch (err) {
+    ctx.scene.leave();
+  }
+};
 
-const confirmarTelefone = new Composer()
+const confirmarTelefone = new Composer();
 confirmarTelefone.action('sim', async (ctx) => {
-        try {
-            await ctx.answerCbQuery()
-            return await confirmacaoPositiva(ctx)
-        } catch (err) {
-            log(err)
-        }
-})
+  try {
+    await ctx.answerCbQuery();
+    return await confirmacaoPositiva(ctx);
+  } catch (err) {
+    log(err);
+  }
+});
 confirmarTelefone.action('nao', async (ctx) => {
-    try {
-        await ctx.answerCbQuery()
-        await ctx.reply(`Por favor, insira seu telefone novamente`)
-        return ctx.wizard.back()
-    } catch (err) {
-        log(err)
-    }
-})
+  try {
+    await ctx.answerCbQuery();
+    await ctx.reply('Por favor, insira seu telefone novamente');
+    return ctx.wizard.back();
+  } catch (err) {
+    log(err);
+  }
+});
 confirmarTelefone.use(async (ctx) => {
-    log(`Sim/Não`)
-    if (confirmado(ctx)) {
-        try {
-            return await confirmacaoPositiva(ctx)
-        } catch (err) {
-            log(err)
-        }
+  log('Sim/Não');
+  if (confirmado(ctx)) {
+    try {
+      return await confirmacaoPositiva(ctx);
+    } catch (err) {
+      log(err);
     }
-    if (negado(ctx)) {
-        try {
-            await ctx.reply(`Por favor, insira seu telefone novamente`)
-            return ctx.wizard.back()
-        } catch (err) {
-            log(err)
-        }
+  }
+  if (negado(ctx)) {
+    try {
+      await ctx.reply('Por favor, insira seu telefone novamente');
+      return ctx.wizard.back();
+    } catch (err) {
+      log(err);
     }
-    await ctx.reply('Por favor, escolha uma das opções.')
-})
+  }
+  await ctx.reply('Por favor, escolha uma das opções.');
+});
 
-module.exports = { pegarTelefone, confirmarTelefone }
+module.exports = { pegarTelefone, confirmarTelefone };
