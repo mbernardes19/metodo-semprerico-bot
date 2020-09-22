b74522cf7b01c456a894c95f64fb96b17445478a
mbernardes19 <bernardes.matheus@outlook.com>
2020-09-22 16:53:25 -0300

    Corrige forma de validar compra de usuário

--- src/passos/planoPago/nomeCompleto.js
+++ src/passos/planoPago/nomeCompleto.js
@@ -1,64 +1,72 @@
-const Composer = require('telegraf/composer')
-const Markup = require('telegraf/markup')
-const Extra = require('telegraf/extra')
-const { log } = require('../../servicos/logger')
-const { confirmado, negado } = require('../../servicos/validacao')
+const Composer = require('telegraf/composer');
+const Markup = require('telegraf/markup');
+const Extra = require('telegraf/extra');
+const { log } = require('../../servicos/logger');
+const { confirmado, negado } = require('../../servicos/validacao');
 
 const pegarNome = async (ctx) => {
-    if (!ctx.message) {
-        await ctx.answerCbQuery()
-    }
-    try {
-        const confirmacao = Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')])
-        await ctx.reply(`Confirmando, seu nome completo é ${ctx.message.text}, certo?`, Extra.inReplyTo(ctx.message.message_id).markup(confirmacao))
-        ctx.wizard.state.novoUsuario.nomeCompleto = ctx.message.text
-        log(`Nome completo definido`)
-        log(ctx.wizard.state.novoUsuario)
-        return ctx.wizard.next()
-    } catch (err) {
-        log(err)
-        ctx.scene.leave()
-    }
-}
+  let mensagem;
+  let textoMensagem;
+  if (!ctx.message) {
+    textoMensagem = ctx.update.message.text;
+    mensagem = ctx.update.message;
+    await ctx.answerCbQuery();
+  } else {
+    textoMensagem = ctx.message.text;
+    mensagem = ctx.message;
+  }
+  try {
+    const confirmacao = Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')]);
+    await ctx.reply(`Confirmando, seu nome completo é ${textoMensagem}, certo?`, Extra.inReplyTo(mensagem.message_id).markup(confirmacao));
+    ctx.wizard.state.novoUsuario.nomeCompleto = textoMensagem;
+    log('Nome completo definido');
+    log(ctx.wizard.state.novoUsuario);
+    return ctx.wizard.next();
+  } catch (err) {
+    log(err);
+    await ctx.reply('Perdão! Ocorreu um erro inesperado agora. Por favor, inicie uma conversa comigo novamente com o comando /start');
+    ctx.scene.leave();
+  }
+};
 
-const confirmarNome = new Composer()
+const confirmarNome = new Composer();
 confirmarNome.action('sim', async (ctx) => {
-    try {
-        await ctx.answerCbQuery()
-        await ctx.reply(`Qual é o seu telefone?`)
-        return ctx.wizard.next()
-    } catch (err) {
-        log(err)
-    }
-})
+  try {
+    await ctx.answerCbQuery();
+    await ctx.reply('Qual é o seu telefone?');
+    return ctx.wizard.next();
+  } catch (err) {
+    log(err);
+  }
+});
 confirmarNome.action('nao', async (ctx) => {
-    try {
-        await ctx.answerCbQuery()
-        await ctx.reply(`Por favor, insira seu nome completo novamente`)
-        return ctx.wizard.back()
-    } catch (err) {
-        log(err)
-    }
-})
+  try {
+    await ctx.answerCbQuery();
+    await ctx.reply('Por favor, insira seu nome completo novamente');
+    return ctx.wizard.back();
+  } catch (err) {
+    log(err);
+  }
+});
 confirmarNome.use(async (ctx) => {
-    log(`Sim/Não`)
-    if (confirmado(ctx)) {
-        try {
-            await ctx.reply(`Qual é o seu telefone?`)
-            return ctx.wizard.next()
-        } catch (err) {
-            log(err)
-        }
+  log('Sim/Não');
+  if (confirmado(ctx)) {
+    try {
+      await ctx.reply('Qual é o seu telefone?');
+      return ctx.wizard.next();
+    } catch (err) {
+      log(err);
     }
-    if (negado(ctx)) {
-        try {
-            await ctx.reply(`Por favor, insira seu nome completo novamente`)
-            return ctx.wizard.back()
-        } catch (err) {
-            log(err)
-        }    
+  }
+  if (negado(ctx)) {
+    try {
+      await ctx.reply('Por favor, insira seu nome completo novamente');
+      return ctx.wizard.back();
+    } catch (err) {
+      log(err);
     }
-    await ctx.reply('Por favor, escolha uma das opções.')
-})
+  }
+  await ctx.reply('Por favor, escolha uma das opções.');
+});
 
-module.exports = { pegarNome, confirmarNome }
+module.exports = { pegarNome, confirmarNome };
--- src/servicos/monetizze.js
+++ src/servicos/monetizze.js
@@ -1,107 +1,113 @@
-const {pegarTransacaoNaMonetizze} = require('./request')
-const regex = require('../utils/regex')
-const dao = require('../dao')
-const { conexaoDb } = require('../db')
-const { log } = require('./logger')
+const { pegarTransacaoNaMonetizze } = require('./request');
+const regex = require('../utils/regex');
+const dao = require('../dao');
+const { conexaoDb } = require('../db');
+const { log } = require('./logger');
 
 const verificarCompraDeUsuarioNaMonetizze = async (ctx) => {
-    const { email, formaDePagamento } = await ctx.wizard.state.novoUsuario
-    const pagamento = regex.CARTAO.test(formaDePagamento) ? 1 : 3
-    try {
-        const response = await pegarTransacaoNaMonetizze({
-            product: process.env.ID_PRODUTO, email, "status[]": 2
-        })
-        log(`Verificando compra de usuário na Monetizze ${JSON.stringify(response)}`)
-        return response.recordCount == 0 ? false : true
-    } catch (err) {
-        throw err
+  const { email } = await ctx.wizard.state.novoUsuario;
+  try {
+    log(`Verificando compra de usuário na Monetizze ${JSON.stringify(response)}`);
+    const response = await pegarTransacaoNaMonetizze({
+      product: process.env.ID_PRODUTO, email, 'status[]': 2,
+    });
+    if (response.recordCount != 0) {
+      return true;
+    } else {
+      const response = await pegarTransacaoNaMonetizze({
+        product: process.env.ID_PRODUTO, email, 'status[]': 6,
+      });
+      if (response.recordCount != 0) {
+        return true;
+      }
+      return false;
     }
-}
+  } catch (err) {
+    throw err;
+  }
+};
 
 const atualizarStatusDeAssinaturaDeUsuarios = async (usuarios) => {
-    const novosStatusAsync = []
-    usuarios.forEach(usuario => novosStatusAsync.push(pegarNovoStatusDeAssinaturaDeUsuario(usuario)))
-    try {
-        const novosStatus = await Promise.all(novosStatusAsync)
-        await dao.atualizarStatusDeAssinaturaDeUsuarios(usuarios, novosStatus, conexaoDb)
-        log(usuarios.map(usuario => usuario.nome_completo))
-        log(`Status de assinatura de todos os usuários atualizado`)
-    } catch (err) {
-        throw err
-    }
-} 
+  const novosStatusAsync = [];
+  usuarios.forEach((usuario) => novosStatusAsync.push(pegarNovoStatusDeAssinaturaDeUsuario(usuario)));
+  try {
+    const novosStatus = await Promise.all(novosStatusAsync);
+    await dao.atualizarStatusDeAssinaturaDeUsuarios(usuarios, novosStatus, conexaoDb);
+    log(usuarios.map((usuario) => usuario.nome_completo));
+    log('Status de assinatura de todos os usuários atualizado');
+  } catch (err) {
+    throw err;
+  }
+};
 
 const pegarNovoStatusDeAssinaturaDeUsuario = async (usuario) => {
-    const { email, status_assinatura } = usuario
-    try {
-        const response = await pegarTransacaoNaMonetizze({
-            product: process.env.ID_PRODUTO, email
-        })
-        if (response.recordCount > 0) {
-            return response.dados[0].assinatura.status.toLowerCase().replace(/ /g, "_")
-        }
-        return status_assinatura
-    } catch (err) {
-        throw err
+  const { email, status_assinatura } = usuario;
+  try {
+    const response = await pegarTransacaoNaMonetizze({
+      product: process.env.ID_PRODUTO, email,
+    });
+    if (response.recordCount > 0) {
+      return response.dados[0].assinatura.status.toLowerCase().replace(/ /g, '_');
     }
-}
+    return status_assinatura;
+  } catch (err) {
+    throw err;
+  }
+};
 const mandarAvisoDeBanimento = async (usuario, telegramClient) => {
-    await dao.aumentarAvisoDeBanimento(usuario, conexaoDb)
-    const usuarioAtualizado = await dao.pegarUsuarioPeloId(usuario.id, conexaoDb);
-    if (usuarioAtualizado.aviso_banimento === 1) {
-        return telegramClient.sendMessage(usuario.id, `A sua assinatura no curso da Monetizze não está como ativa. Procure realizar o pagamento do curso para evitar seu banimento dos nossos canais em 2 dias. Caso já tenha realizado o pagamento desse mês, favor ignorar esta mensagem`)
-    }
-    if (usuarioAtualizado.aviso_banimento === 2) {
-        return telegramClient.sendMessage(usuario.id, `A sua assinatura no curso da Monetizze não está como ativa há 1 dia. Procure realizar o pagamento do curso para evitar seu banimento dos nossos canais amanhã. Caso já tenha realizado o pagamento desse mês, favor ignorar esta mensagem`)
-    }
-}
+  await dao.aumentarAvisoDeBanimento(usuario, conexaoDb);
+  const usuarioAtualizado = await dao.pegarUsuarioPeloId(usuario.id, conexaoDb);
+  if (usuarioAtualizado.aviso_banimento === 1) {
+    return telegramClient.sendMessage(usuario.id, 'A sua assinatura no curso da Monetizze não está como ativa. Procure realizar o pagamento do curso para evitar seu banimento dos nossos canais em 2 dias. Caso já tenha realizado o pagamento desse mês, favor ignorar esta mensagem');
+  }
+  if (usuarioAtualizado.aviso_banimento === 2) {
+    return telegramClient.sendMessage(usuario.id, 'A sua assinatura no curso da Monetizze não está como ativa há 1 dia. Procure realizar o pagamento do curso para evitar seu banimento dos nossos canais amanhã. Caso já tenha realizado o pagamento desse mês, favor ignorar esta mensagem');
+  }
+};
 
 const banirUsuariosSeStatusNaoForAtivo = async (usuarios, telegramClient) => {
-    const usuariosASeremBanidos = []
-    const usuariosASeremAvisados = []
-    const usuariosATeremAvisosZerados = []
-    const dadosUsuariosASeremBanidos = []
-    const dadosUsuariosAvisados = []
-    const dadosUsuariosComAvisosZerados = []
+  const usuariosASeremBanidos = [];
+  const usuariosASeremAvisados = [];
+  const usuariosATeremAvisosZerados = [];
+  const dadosUsuariosASeremBanidos = [];
+  const dadosUsuariosAvisados = [];
+  const dadosUsuariosComAvisosZerados = [];
 
-    usuarios.forEach(usuario => {
-        if (usuario.status_assinatura !== 'ativa') {
-            if (usuario.aviso_banimento > 2) {
-                if (process.env.NODE_ENV === 'development') {
-                    usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_TESTE, usuario.id))
-                    usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_TESTE, usuario.id))
-                } else {
-                    usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_RICO_VIDENTE, usuario.id))
-                    usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_SINAIS_RICOS, usuario.id))
-                }
-                dadosUsuariosASeremBanidos.push(usuario.nome_completo)
-                return
-            }
-            usuariosASeremAvisados.push(mandarAvisoDeBanimento(usuario, telegramClient))
-            dadosUsuariosAvisados.push(usuario.nome_completo)
-            return
+  usuarios.forEach((usuario) => {
+    if (usuario.status_assinatura !== 'ativa') {
+      if (usuario.aviso_banimento > 2) {
+        if (process.env.NODE_ENV === 'development') {
+          usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_TESTE, usuario.id));
+          usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_TESTE, usuario.id));
         } else {
-            if (usuario.aviso_banimento > 0) {
-                usuariosATeremAvisosZerados.push(dao.zerarAvisoDeBanimento(usuario, conexaoDb))
-                dadosUsuariosComAvisosZerados.push(usuario.nome_completo)
-            }
+          usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_RICO_VIDENTE, usuario.id));
+          usuariosASeremBanidos.push(telegramClient.kickChatMember(process.env.ID_CANAL_SINAIS_RICOS, usuario.id));
         }
-    })
-
-    try {
-        await Promise.all(usuariosASeremBanidos)
-        log(`Usuários ${dadosUsuariosASeremBanidos} foram banidos`)
-        await Promise.all(usuariosASeremAvisados)
-        log(`Usuários ${dadosUsuariosAvisados} avisados sobre banimento`)
-        await Promise.all(usuariosATeremAvisosZerados)
-        log(`Usuários ${dadosUsuariosComAvisosZerados} tiveram aviso de banimento zerado`)
-    } catch (err) {
-        throw err
+        dadosUsuariosASeremBanidos.push(usuario.nome_completo);
+        return;
+      }
+      usuariosASeremAvisados.push(mandarAvisoDeBanimento(usuario, telegramClient));
+      dadosUsuariosAvisados.push(usuario.nome_completo);
+    } else if (usuario.aviso_banimento > 0) {
+      usuariosATeremAvisosZerados.push(dao.zerarAvisoDeBanimento(usuario, conexaoDb));
+      dadosUsuariosComAvisosZerados.push(usuario.nome_completo);
     }
-} 
+  });
 
-module.exports = { 
-    verificarCompraDeUsuarioNaMonetizze,
-    atualizarStatusDeAssinaturaDeUsuarios,
-    banirUsuariosSeStatusNaoForAtivo
-}
+  try {
+    await Promise.all(usuariosASeremBanidos);
+    log(`Usuários ${dadosUsuariosASeremBanidos} foram banidos`);
+    await Promise.all(usuariosASeremAvisados);
+    log(`Usuários ${dadosUsuariosAvisados} avisados sobre banimento`);
+    await Promise.all(usuariosATeremAvisosZerados);
+    log(`Usuários ${dadosUsuariosComAvisosZerados} tiveram aviso de banimento zerado`);
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  verificarCompraDeUsuarioNaMonetizze,
+  atualizarStatusDeAssinaturaDeUsuarios,
+  banirUsuariosSeStatusNaoForAtivo,
+};
