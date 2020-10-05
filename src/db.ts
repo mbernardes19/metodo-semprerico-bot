6d52d148200c315a9d17ed77b6b19750486d4ecf
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-05 19:04:18 -0300

    Changes db config from js to ts

---
+++ src/db.ts
@@ -0,0 +1,22 @@
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+import path from 'path';
+if(process.env.NODE_ENV === 'production') {
+    dotenv.config({path: path.join(__dirname, '..', '.env') })
+} else {
+    dotenv.config({path: path.join(__dirname, '..', '.env.test') })
+}
+
+
+console.log('SENHA', process.env.DB_PASSWORD)
+
+const dbConnection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_HOST_PORT, 10),
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dateStrings: true,
+})
+
+export { dbConnection as conexaoDb }
