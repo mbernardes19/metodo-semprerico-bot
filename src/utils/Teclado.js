8454a72c0a813cc364b658afc30290354807186e
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-09 23:32:12 -0300

    Removes free plan button

--- src/utils/Teclado.js
+++ src/utils/Teclado.js
@@ -8,7 +8,7 @@ const Teclado = {
     FORMAS_DE_PAGAMENTO_GRATUITO: Markup.inlineKeyboard([
         [Markup.callbackButton('💳 Cartão de Crédito', 'cartao_de_credito')],
         [Markup.callbackButton('📄 Boleto', 'boleto')],
-        [Markup.callbackButton('🆓 Plano Gratuito', 'plano_gratuito')]
+        // [Markup.callbackButton('🆓 Plano Gratuito', 'plano_gratuito')]
       ]),
     CONFIRMACAO: Markup.inlineKeyboard([Markup.callbackButton('👍 Sim', 'sim'), Markup.callbackButton('👎 Não', 'nao')])
 }
