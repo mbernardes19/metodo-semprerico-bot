b345e1080522d5d55562d230243825553167193c
mbernardes19 <bernardes.matheus@outlook.com>
2020-10-31 16:39:06 -0300

    Adds health checker for test app

---
+++ health-checker-teste.js
@@ -0,0 +1,27 @@
+const app = require('express')()
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+async function runDeploy() {
+    try {
+        const {stdout, stderr} = await exec('pm2 restart app-teste')
+        console.log('stdout', stdout)
+        console.log('stderr', stderr)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+app.get('/', (req, res) => {
+    res.send('Hello SEMPRE RICO TESTE').status(200)
+})
+app.get('/HealthTeste', (req, res) => {
+    res.send('Hello SEMPRE RICO TESTE').status(200)
+})
+app.get('/HealthTeste/revive', async (req, res) => {
+    await runDeploy()
+    res.sendStatus(200);
+})
+
+const PORT = process.env.PORT_METODO_SEMPRERICO_BOT_HEALTH_CHECKER_TESTE || process.env.PORT_APP || 21563 || 3000
+app.listen(PORT, () => console.log(`Health checker rodando na porta ${PORT}`));
