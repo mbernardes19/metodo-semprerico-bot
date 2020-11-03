cc45bb86f093b97b42d680dbdb6c0362f76e633c
mbernardes19 <bernardes.matheus@outlook.com>
2020-11-03 18:59:46 -0300

    Adds teste endpoints

--- src/model/ExpressServer.ts
+++ src/model/ExpressServer.ts
@@ -55,9 +55,12 @@ export default class ExpressServer {
         this._express.get('/Teste', (req, res) => {
           res.status(200).send('<h1>Olá App Teste!</h1>');
         })
-        this.startTradingApiEndpoints()
+        this.startTradingApiEndpoints();
+        this.startTradingApiEndpointsTest();
         this.startMessageEditingEndpointsRicoVidente();
+        this.startMessageEditingEndpointsRicoVidenteTest();
         this.startMessageEditingEndpointsSinaisRicos();
+        this.startMessageEditingEndpointsSinaisRicosTest();
     }
 
     getExpressServer() {
@@ -162,6 +165,101 @@ export default class ExpressServer {
             res.status(200).send();
           });
     }
+
+    private startTradingApiEndpointsTest() {
+      this._express.post('/Teste/operation-result', async (req, res) => {
+          const telegramClient = this._bot.getTelegramClient();
+          const channelMessageId = req.body.telegramMessageId
+          const channelToSend = req.body.telegramChannelId;
+          const hasGale = req.body.gale;
+        
+          if (!hasGale) {
+            log('NO GALE')
+            const winMessage = '✅\n';
+            const lossMessage = '❎\n';
+            const closedMessage = '🔒\n';
+            const notInStrategyMessage = '💥\n';
+        
+            console.log('MESSAGE ID', channelMessageId)
+            console.log('CHANNEL TO SEND', channelToSend)
+          
+            const resultadoOperacao = req.body
+            let mensagem = '⚡💰⚡\n';
+            resultadoOperacao.results.forEach(r => {
+              if (r.result === 'WIN') {
+                log('WIN');
+                mensagem += winMessage;
+                return;
+              }
+              if (r.result === 'LOSS') {
+                log('LOSS');
+                mensagem += lossMessage;
+                return;
+              }
+              if (r.result === 'DOJI') {
+                log('DOJI LOSS');
+                mensagem += lossMessage;
+                return;
+              }
+              if (r.result === '') {
+                log('DOJI LOSS');
+                mensagem += closedMessage;
+                return;
+              }
+              if (r.result === 'NOT IN STRATEGY') {
+                log('DOJI LOSS');
+                mensagem += notInStrategyMessage;
+                return;
+              }
+            })
+            res.status(200).send({message: `Operation result sent to channel ${channelToSend}`, messageToReply: channelMessageId});
+            await telegramClient.sendMessage(channelToSend, mensagem, Extra.inReplyTo(channelMessageId));
+            return;
+          }
+
+          log('GALE')
+        
+          console.log('MESSAGE ID',channelMessageId)
+          console.log('CHANNEL TO SEND', channelToSend)
+        
+          const resultadoOperacao = req.body
+          if (resultadoOperacao.results[0].result === 'WIN') {
+            const [MENSAGEM_WIN] = await dao.pegarMensagem(channelToSend, 'win', conexaoDb);
+            const [STICKER_WIN] = await dao.pegarSticker(channelToSend, 'win', conexaoDb);
+            log('WIN');
+            res.status(200).send({message: `Operation result sent to channel ${channelToSend}`, messageToReply: channelMessageId});
+            await telegramClient.sendMessage(channelToSend, MENSAGEM_WIN.texto, Extra.inReplyTo(channelMessageId));
+            await telegramClient.sendSticker(channelToSend, STICKER_WIN.texto, Extra.inReplyTo(channelMessageId));
+            return;
+          }
+          if (resultadoOperacao.results[0].result === 'LOSS') {
+            const [MENSAGEM_LOSS] = await dao.pegarMensagem(channelToSend, 'loss', conexaoDb);
+            const [STICKER_LOSS] = await dao.pegarSticker(channelToSend, 'loss', conexaoDb);
+            log('LOSS');
+            res.status(200).send({message: `Operation result sent to channel ${channelToSend}`, messageToReply: channelMessageId});
+            await telegramClient.sendMessage(channelToSend, MENSAGEM_LOSS.texto, Extra.inReplyTo(channelMessageId));
+            await telegramClient.sendSticker(channelToSend, STICKER_LOSS.texto, Extra.inReplyTo(channelMessageId));
+            return;
+          }
+          if (resultadoOperacao.result[0].result === 'DOJI') {
+            const [MENSAGEM_LOSS] = await dao.pegarMensagem(channelToSend, 'loss', conexaoDb);
+            const [STICKER_LOSS] = await dao.pegarSticker(channelToSend, 'loss', conexaoDb);
+            const [MENSAGEM_DOJI] = await dao.pegarMensagem(channelToSend, 'doji', conexaoDb);
+            log('DOJI LOSS');
+            res.status(200).send({message: `Operation result sent to channel ${channelToSend}`, messageToReply: channelMessageId});
+            await telegramClient.sendMessage(channelToSend, MENSAGEM_LOSS.texto, Extra.inReplyTo(channelMessageId));
+            await telegramClient.sendSticker(channelToSend, STICKER_LOSS.texto, Extra.inReplyTo(channelMessageId));
+            await telegramClient.sendMessage(channelToSend, MENSAGEM_DOJI.texto, Extra.inReplyTo(channelMessageId));
+            return;
+          }
+        });
+        
+        this._express.post('/Teste/signal-failed', async (req, res) => {
+          await this._bot.getTelegramClient().sendMessage(721557882, req.body.message)
+          await this._bot.getTelegramClient().sendMessage(923769783, req.body.message)
+          res.status(200).send();
+        });
+  }
 
     private startMessageEditingEndpointsRicoVidente() {
         this._express.post('/App/RV-mensagem-win', async (req, res) => {
@@ -455,5 +553,299 @@ export default class ExpressServer {
             res.sendStatus(500);
           }
         });
+  }
+
+    private startMessageEditingEndpointsRicoVidenteTest() {
+      this._express.post('/Teste/RV-mensagem-win', async (req, res) => {
+          log(req.body);
+          const { mensagemWin } = req.body;
+          try {
+            await dao.atualizarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'win', mensagemWin, conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.sendStatus(200);
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.post('/Teste/RV-sticker-win', async (req, res) => {
+          const { stickerWin } = req.body;
+          try {
+            await dao.atualizarSticker(process.env.ID_CANAL_RICO_VIDENTE, 'win', stickerWin, conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.sendStatus(200);
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.post('/Teste/RV-mensagem-loss', async (req, res) => {
+          const { mensagemLoss } = req.body;
+          try {
+            await dao.atualizarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'loss', mensagemLoss, conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.sendStatus(200);
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.post('/Teste/RV-sticker-loss', async (req, res) => {
+          const { stickerLoss } = req.body;
+          try {
+            await dao.atualizarSticker(process.env.ID_CANAL_RICO_VIDENTE, 'loss', stickerLoss, conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.sendStatus(200);
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.post('/Teste/RV-mensagem-doji', async (req, res) => {
+          const { mensagemDoji } = req.body;
+          try {
+            await dao.atualizarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'doji', mensagemDoji, conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.sendStatus(200);
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.get('/Teste/RV-mensagem-win', async (req, res) => {
+          try {
+            const [mensagemWin] = await dao.pegarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'win', conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).json({ id: mensagemWin.id, texto: mensagemWin.texto });
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.get('/Teste/RV-sticker-win', async (req, res) => {
+          try {
+            const [stickerWin] = await dao.pegarSticker(process.env.ID_CANAL_RICO_VIDENTE, 'win', conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).json({ id: stickerWin.id, texto: stickerWin.texto });
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.get('/Teste/RV-mensagem-loss', async (req, res) => {
+          try {
+            const [mensagemLoss] = await dao.pegarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'loss', conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).json({ id: mensagemLoss.id, texto: mensagemLoss.texto });
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            log(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.get('/Teste/RV-sticker-loss', async (req, res) => {
+          try {
+            const [stickerLoss] = await dao.pegarSticker(process.env.ID_CANAL_RICO_VIDENTE, 'loss', conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).json({ id: stickerLoss.id, texto: stickerLoss.texto });
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            res.sendStatus(500);
+          }
+        });
+        
+        this._express.get('/Teste/RV-mensagem-doji', async (req, res) => {
+          try {
+            const [mensagemDoji] = await dao.pegarMensagem(process.env.ID_CANAL_RICO_VIDENTE, 'doji', conexaoDb);
+            res.set('Access-Control-Allow-Origin', '*');
+            res.status(200).json({ id: mensagemDoji.id, texto: mensagemDoji.texto });
+          } catch (err) {
+            await enviarEmailDeRelatorioDeErro(err);
+            res.sendStatus(500);
+          }
+        });
+  }
+
+    private startMessageEditingEndpointsSinaisRicosTest() {
+    this._express.post('/Teste/SR-mensagem-win', async (req, res) => {
+        log(req.body);
+        const { mensagemWin } = req.body;
+        try {
+          await dao.atualizarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'win', mensagemWin, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.post('/Teste/SR-sticker-win', async (req, res) => {
+        const { stickerWin } = req.body;
+        try {
+          await dao.atualizarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'win', stickerWin, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.post('/Teste/SR-mensagem-loss', async (req, res) => {
+        const { mensagemLoss } = req.body;
+        try {
+          await dao.atualizarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'loss', mensagemLoss, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.post('/Teste/SR-sticker-loss', async (req, res) => {
+        const { stickerLoss } = req.body;
+        try {
+          await dao.atualizarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'loss', stickerLoss, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.post('/Teste/SR-mensagem-doji', async (req, res) => {
+        const { mensagemDoji } = req.body;
+        try {
+          await dao.atualizarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'doji', mensagemDoji, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+
+      this._express.post('/Teste/SR-mensagem-automatica', async (req, res) => {
+        const { mensagemAutomatica } = req.body;
+        try {
+          await dao.atualizarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'automatica', mensagemAutomatica, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+
+      this._express.post('/Teste/SR-sticker-automatico', async (req, res) => {
+        const { stickerAutomatico } = req.body;
+        try {
+          await dao.atualizarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'automatico', stickerAutomatico, conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.sendStatus(200);
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.get('/Teste/SR-mensagem-win', async (req, res) => {
+        try {
+          const [mensagemWin] = await dao.pegarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'win', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: mensagemWin.id, texto: mensagemWin.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.get('/Teste/SR-sticker-win', async (req, res) => {
+        try {
+          const [stickerWin] = await dao.pegarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'win', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: stickerWin.id, texto: stickerWin.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.get('/Teste/SR-mensagem-loss', async (req, res) => {
+        try {
+          const [mensagemLoss] = await dao.pegarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'loss', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: mensagemLoss.id, texto: mensagemLoss.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          log(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.get('/Teste/SR-sticker-loss', async (req, res) => {
+        try {
+          const [stickerLoss] = await dao.pegarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'loss', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: stickerLoss.id, texto: stickerLoss.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+      
+      this._express.get('/Teste/SR-mensagem-doji', async (req, res) => {
+        try {
+          const [mensagemDoji] = await dao.pegarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'doji', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: mensagemDoji.id, texto: mensagemDoji.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+
+      this._express.get('/Teste/SR-mensagem-automatica', async (req, res) => {
+        try {
+          const [mensagemAutomatica] = await dao.pegarMensagem(process.env.ID_CANAL_SINAIS_RICOS, 'automatica', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: mensagemAutomatica.id, texto: mensagemAutomatica.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
+
+      this._express.get('/Teste/SR-sticker-automatico', async (req, res) => {
+        try {
+          const [stickerAutomatico] = await dao.pegarSticker(process.env.ID_CANAL_SINAIS_RICOS, 'automatico', conexaoDb);
+          res.set('Access-Control-Allow-Origin', '*');
+          res.status(200).json({ id: stickerAutomatico.id, texto: stickerAutomatico.texto });
+        } catch (err) {
+          await enviarEmailDeRelatorioDeErro(err);
+          res.sendStatus(500);
+        }
+      });
   }
 }
